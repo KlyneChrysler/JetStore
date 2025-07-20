08df7baa1a09a17db7c515b4b33a20ac636d339c
Klyne Chrysler <klynechrislu@gmail.com>
2025-07-20 16:39:44 +0800

    changes port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // authentication
 app.use("/api/auth", authRoutes);
