1fdef11fc88a11abf9fae3c62d547d85491d9aca
Klyne Chrysler <klynechrislu@gmail.com>
2025-07-20 16:53:31 +0800

    polished

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("[ SIGN UP ROUTE CALLED ]");
+};
+
+export const login = async (req, res) => {
+  res.send("[ LOGIN ROUTE CALLED ]");
+};
+
+export const logout = async (req, res) => {
+  res.send("[ LOGOUT ROUTE CALLED ]");
+};
--- backend/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/signup", (req, res) => {
-  res.send("[ SIGN UP ROUTE CALLED ]");
-});
-
-export default router;
