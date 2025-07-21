250f94cac787aa4be0a50b15df4c619d950a0c75
Klyne Chrysler <klynechrislu@gmail.com>
2025-07-21 22:58:45 +0800

    same

---
+++ backend/models/product.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+// productSchema for database
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: [true, "Image is required"],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true } // createdAt, updatedAt
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
